1cf2148f3b96ae45a5485c95700bff6a6bd07d94
Eduardo Melo <eduardo.melo@empiricus.com.br>
2020-06-25 19:02:56 -0300

    initial version

---
+++ src/main/java/br/com/dudzdev/me/MeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dudzdev.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dudzdev/me/MeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dudzdev.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
