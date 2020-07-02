eee1b63631ea1eaee19693964486c6b55ec66fcf
Eduardo Melo <dudz.dev@gmail.com>
2020-07-01 23:11:34 -0300

    update classes for liquibase and propeties postgres

--- src/main/java/br/com/dudzdev/me/MeServiceApplication.java
+++ src/main/java/br/com/dudzdev/me/MeServiceApplication.java
@@ -2,12 +2,15 @@ package br.com.dudzdev.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class MeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeServiceApplication.class, args);
+    }
 
 }
